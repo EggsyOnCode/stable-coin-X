485fb81fd35cd1a211addc6e9ab6c73dc107c96d
EggsyOnCode <farzansaqib2003@gmail.com>
2024-06-06 06:26:47 +0500

    func for liquidations added and some tests

--- src/XEngine.sol
+++ src/XEngine.sol
@@ -58,6 +58,8 @@ contract XEngine is ReentrancyGuard {
     error XEngine_TransferFailed();
     error XEngine_MintFailed();
     error XEngine_BreaksHealthFactor();
+    error XEngine_HealthFactorOk();
+    error XEngine_HealthFactorNotImproved();
     ///////
     //StateVars///
     ///////
@@ -72,12 +74,13 @@ contract XEngine is ReentrancyGuard {
     uint256 private constant ADDITIONAL_FEED_PRECISION = 1e10;
     uint256 private constant LIQUIDATION_THRESHOLD = 50; //200% overcollateralized
     uint256 private constant LIQUIDATION_PRECISION = 100;
+    uint256 private constant LIQUIDATION_BONUS = 10; //rep 10%
     uint256 private constant MIN_HEALTH_FACTOR = 1;
 
     ///////
     //Events//
     ///////
-    event CollateralDepsited(address indexed user, address indexed jtoken, uint256 indexed amt);
+    event CollateralDepsited(address indexed user, address to, address indexed jtoken, uint256 indexed amt);
 
     ///////
     //Modifiers///
@@ -128,7 +131,7 @@ contract XEngine is ReentrancyGuard {
         //register the collateral deposited
         s_collateralDeposited[msg.sender][tokenCollateralAddress] += collateralAmt;
 
-        emit CollateralDepsited(msg.sender, tokenCollateralAddress, collateralAmt);
+        emit CollateralDepsited(msg.sender, msg.sender, tokenCollateralAddress, collateralAmt);
 
         //interface for the ERC20 token used as adapter over the tokenCollateralAddress to invoke ERC20 specific func over it
         bool suc = IERC20(tokenCollateralAddress).transferFrom(msg.sender, address(this), collateralAmt);
@@ -144,25 +147,18 @@ contract XEngine is ReentrancyGuard {
         mintX(amtMnt);
     }
 
+    // 100 $ ETH --> depsited as collateral
+    // 20 $ X minted
+    // FOR redeeming now; you must 1. burn 20 X 2. redeem 100$ ETH cuz we can never be undercollateralized at any time
     /// @notice Redeems the collateral deposited by the user
     function redeemCollateral(address tokenCollateralAddress, uint256 collateralAmt)
         public
         nonReentrant
         moreThanZero(collateralAmt)
     {
-        s_collateralDeposited[msg.sender][tokenCollateralAddress] -= collateralAmt;
-        emit CollateralDepsited(msg.sender, tokenCollateralAddress, collateralAmt);
-        bool success = IERC20(tokenCollateralAddress).transfer(msg.sender, collateralAmt);
-        if (!success) {
-            revert XEngine_TransferFailed();
-        }
-        //  check if health factor is  not broken
+        _redeemCollateral(tokenCollateralAddress, collateralAmt, msg.sender, msg.sender);
         _revertIfHealthFactorIsBroken(msg.sender);
     }
-
-    // 100 $ ETH --> depsited as collateral
-    // 20 $ X minted
-    // FOR redeeming now; you must 1. burn 20 X 2. redeem 100$ ETH cuz we can never be undercollateralized at any time
 
     /// @param tokenAmt: amount of X to be burnt
     /// @param tokenCollateralAddress: address of the token in which collateral is deposited (ERC20 token)
@@ -176,16 +172,7 @@ contract XEngine is ReentrancyGuard {
     }
 
     function burnX(uint256 amt) public moreThanZero(amt) {
-        s_XMinted[msg.sender] -= amt;
-        // transfering ownership to Engine ; promotng Single Resp Func
-        // Engine should be resp for maanging the funds and not the ERC20 contract
-        // IMP design pricniple
-        bool success = i_XStableCoin.transferFrom(msg.sender, address(this), amt);
-        if (!success) {
-            revert XEngine_TransferFailed();
-        }
-        // burn the X
-        i_XStableCoin.burn(amt);
+        _burnX(amt, msg.sender, msg.sender);
         // check if the user is undercollateralized
         _revertIfHealthFactorIsBroken(msg.sender);
     }
@@ -197,6 +184,61 @@ contract XEngine is ReentrancyGuard {
         _revertIfHealthFactorIsBroken(msg.sender);
         //if the mint fails the tx would be revereted by teh mint function
         i_XStableCoin.mint(msg.sender, amtX);
+    }
+
+    /*
+     * @param collateral: The ERC20 token address of the collateral you're using to make the protocol solvent again. (eg WETH)
+     * This is collateral that you're going to take from the user who is insolvent. 
+     * In return, you have to burn your DSC to pay off their debt, but you don't pay off your own.
+     * @param user: The user who is insolvent. They have to have a _healthFactor below MIN_HEALTH_FACTOR
+     * @param debtToCover: The amount of DSC you want to burn to cover the user's debt.
+     *
+     * @notice: You can partially liquidate a user.
+     * @notice: You will get a 10% LIQUIDATION_BONUS for taking the users funds.
+    * @notice: This function working assumes that the protocol will be roughly 150% overcollateralized in order for this
+    to work.
+    * @notice: A known bug would be if the protocol was only 100% collateralized, we wouldn't be able to liquidate
+    anyone.
+     * For example, if the price of the collateral plummeted before anyone could be liquidated.
+     */
+    function liquidate(address collateral, address user, uint256 debtToCover)
+        external
+        moreThanZero(debtToCover)
+        nonReentrant
+    {
+        //CEI
+
+        // check if the user is liquidatable
+        uint256 startingHealthFactor = _healthFactor(user);
+        if (startingHealthFactor >= MIN_HEALTH_FACTOR) {
+            revert XEngine_HealthFactorOk();
+        }
+
+        // calculate the amount of collateral to be redeemed
+        // if $100 worth of X to be covered then we need 100$ worth of collateral
+        uint256 tokenAmtFromDebtCovered = getTokenAmtFromUsd(collateral, debtToCover);
+        // adding a liquidation bonus as well
+
+        // And give them a 10% bonus
+        // So we are giving the liquidator $110 of WETH for 100 DSC
+        // We should implement a feature to liquidate in the event the protocol is insolvent
+        // And sweep extra amounts into a treasury
+
+        // 0.05 eth * 0.1 = 0.005 ==> 0.055 eth in totalCollateral
+        uint256 bonusCollateralAmt = (tokenAmtFromDebtCovered * LIQUIDATION_BONUS) / LIQUIDATION_PRECISION;
+        uint256 totalCollateral = bonusCollateralAmt + tokenAmtFromDebtCovered;
+
+        _redeemCollateral(collateral, totalCollateral, user, msg.sender);
+        // burn an equivalent DSC of thes msg.sender
+        _burnX(debtToCover, user, msg.sender);
+
+        uint256 endingHf = _healthFactor(user);
+        //if HF hasnt improved at all of the user; our liquidation would be fruitless
+        if (endingHf <= startingHealthFactor) {
+            revert XEngine_HealthFactorNotImproved();
+        }
+        //also revert if liquidating affects the HF of the liquidator
+        _revertIfHealthFactorIsBroken(msg.sender);
     }
 
     /////////////////////PUBLIC and EXTERNAL VIEW FUNCTIONS////////////////////////
@@ -216,12 +258,23 @@ contract XEngine is ReentrancyGuard {
         }
     }
 
+    // @param : usdAmtInWei : amount in USD rep in WEI (like for 100 usd we would pass 100 ether or 100e18)
+    function getTokenAmtFromUsd(address tokenAddr, uint256 usdAmtInWei) public view returns (uint256) {
+        AggregatorV3Interface priceFeed = AggregatorV3Interface(s_priceFeeds[tokenAddr]);
+        (, int256 price,,,) = priceFeed.latestRoundData();
+        // $100e18 USD Debt
+        // 1 ETH = 2000 USD
+        // The returned value from Chainlink will be 2000 * 1e8
+        // Most USD pairs have 8 decimals, so we will just pretend they all do
+        return ((usdAmtInWei * PRECISION) / (uint256(price) * ADDITIONAL_FEED_PRECISION));
+    }
+
     //////////////INTERNAL FUNCTIONS////////////////////
 
     /// @notice returns how close to liquidation a user is
     /// if the health factor is less than 1, the user is undercollateralized hence liquidated
     /// @param user user address
-    function _healthFactor(address user) internal returns (uint256) {
+    function _healthFactor(address user) internal view returns (uint256) {
         // calcaulte total X minted by the user
         // calculate total collateral deposited by the user
         (uint256 totalCollateral, uint256 totalX) = _getUserAccountInfo(user);
@@ -229,7 +282,7 @@ contract XEngine is ReentrancyGuard {
         return (collateralAjustedForThreshold * PRECISION) / totalX;
     }
 
-    function _revertIfHealthFactorIsBroken(address user) internal {
+    function _revertIfHealthFactorIsBroken(address user) internal view {
         // check the heath factor of the user
         // liquidate if below 1
         uint256 userHealthFactor = _healthFactor(user);
@@ -239,11 +292,40 @@ contract XEngine is ReentrancyGuard {
         }
     }
 
-    function _liquidate(address user) internal {}
-
     function _getUserAccountInfo(address user) internal view returns (uint256, uint256) {
         uint256 totalMinted = s_XMinted[user];
         uint256 totalCollateral = getCollateralDeposited(user);
         return (totalMinted, totalCollateral);
+    }
+
+    /// PRIVATE And Internal Functions ////
+
+    /// @notice Redeems the collateral deposited by the user
+    function _redeemCollateral(address tokenCollateralAddress, uint256 collateralAmt, address from, address to)
+        internal
+        nonReentrant
+        moreThanZero(collateralAmt)
+    {
+        s_collateralDeposited[from][tokenCollateralAddress] -= collateralAmt;
+        emit CollateralDepsited(from, to, tokenCollateralAddress, collateralAmt);
+        bool success = IERC20(tokenCollateralAddress).transfer(to, collateralAmt);
+        if (!success) {
+            revert XEngine_TransferFailed();
+        }
+    }
+
+    /// @notice Low level internal func; don;t call unless the calling func is also
+    /// checking the health facotr afterwards
+    function _burnX(uint256 amt, address onBehalfOf, address XFrom) public moreThanZero(amt) {
+        s_XMinted[onBehalfOf] -= amt;
+        // transfering ownership to Engine ; promotng Single Resp Func
+        // Engine should be resp for maanging the funds and not the ERC20 contract
+        // IMP design pricniple
+        bool success = i_XStableCoin.transferFrom(XFrom, address(this), amt);
+        if (!success) {
+            revert XEngine_TransferFailed();
+        }
+        // burn the X
+        i_XStableCoin.burn(amt);
     }
 }
--- test/units/XEngine_test.t.sol
+++ test/units/XEngine_test.t.sol
@@ -13,6 +13,7 @@ contract XEngineTest is Test {
     XStableCoin public stableCoin;
     HelperConfig public helperConfig;
     address public wethPriceFeed;
+    address public bethPriceFeed;
     address public weth;
 
     address public USER = makeAddr("user");
@@ -25,7 +26,7 @@ contract XEngineTest is Test {
 
     //////////// Price Feed Tests ////////////
 
-    function testDepositComplete() public {
+    function testDepositComplete() public view {
         //setUp
         uint256 amount = 15e18; // 15 ethers in wei
         // 15e18 * 2000/ether = 30000e18
@@ -34,6 +35,27 @@ contract XEngineTest is Test {
         uint256 actualUsd = engine._getUsdPrice(weth, amount);
         //assert
         assertEq(actualUsd, expectedUsdPrice);
+    }
+
+    //// Constructor Tests /////
+    address[] public tokenAddrs;
+    address[] public priceFeeds;
+
+    function testConstructor() public {
+        //setUp
+        tokenAddrs.push(weth);
+        priceFeeds.push(wethPriceFeed);
+        priceFeeds.push(bethPriceFeed);
+        //execution
+        vm.expectRevert();
+        new XEngine(tokenAddrs, priceFeeds, address(stableCoin));
+    }
+
+    function testGetTokenAmt() public view {
+        uint256 usd = 100 ether;
+        uint256 expectedWeth = 0.05 ether;
+        uint256 actualWeth = engine.getTokenAmtFromUsd(weth, usd);
+        assertEq(actualWeth, expectedWeth);
     }
 
     //// depositCollateral Tests ////
